0c1b3a011797c01a9e4c4e7f8b255243fb7a92fa
horsefacts <webmaster@terminally.online>
2024-02-08 17:54:55 -0500

    chore: add link to Github

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 A Farcaster frame that demos "mint with Warps" and "claim with Warpcast active badge" actions. Pay gas costs and mint fees for users with an active badge, let everyone else mint with Warps.
 
 Built using open source tools:
+
 - [Zizzamia/a-frame-in-100-lines](https://github.com/Zizzamia/a-frame-in-100-lines)
 - [coinbase/onchainkit](https://github.com/coinbase/onchainkit)
 - [Syndicate Frame API](https://frame.syndicate.io/)
--- app/page.tsx
+++ app/page.tsx
@@ -34,17 +34,24 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function Page() {
   const { name, image, address, tokenId } = await getCollection();
   return (
-    <div className="flex flex-col md:flex-row md:items-center justify-center min-h-screen items-start font-body">
-      <div className="w-full md:w-3/4 flex justify-center items-center">
-        <img src={image} alt={name} className="w-full lg:max-w-[800px] md:max-w-[400px] h-auto" />
-      </div>
-      <div className="w-full md:w-1/4 flex flex-col items-center md:items-start space-y-4 mt-4 md:mt-0 md:pl-4">
-        <h1 className="text-2xl font-bold">{name}</h1>
-        <a href={`https://zora.co/collect/base:${address}/${tokenId}`} target="_blank">
-          <button className="px-4 py-2 bg-violet-500 text-white hover:bg-violet-700 transition duration-300">
-            Mint on Zora
-          </button>
-        </a>
+    <div>
+      <div className="flex flex-col md:flex-row md:items-center justify-center min-h-screen items-start font-body">
+        <div className="w-full md:w-3/4 flex justify-center items-center">
+          <img src={image} alt={name} className="w-full lg:max-w-[800px] md:max-w-[400px] h-auto" />
+        </div>
+        <div className="w-full md:w-1/4 flex flex-col items-center md:items-start space-y-4 mt-4 md:mt-0 md:pl-4">
+          <h1 className="text-2xl font-bold">{name}</h1>
+          <a href={`https://zora.co/collect/base:${address}/${tokenId}`} target="_blank">
+            <button className="px-4 py-2 bg-violet-500 text-white hover:bg-violet-700 transition duration-300">
+              Mint on Zora
+            </button>
+          </a>
+          <div className="text-xs text-stone-400 hover:underline tracking-tighter text-center">
+            <a href="https://github.com/horsefacts/base-mint-with-warps" target="_blank">
+              See code on Github
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
