5e9f3423f56611cb82eac85c4ec475cd93b2f3c1
horsefacts <webmaster@terminally.online>
2024-02-08 21:09:08 -0500

    fix: log HTML response

--- app/lib/getFrameHtml.ts
+++ app/lib/getFrameHtml.ts
@@ -3,5 +3,7 @@ import { FrameMetadataType, getFrameHtmlResponse } from '@coinbase/onchainkit';
 export function getFrameHtml(frameMetadata: FrameMetadataType) {
   const html = getFrameHtmlResponse(frameMetadata);
   // hack: remove close tags and add aspect ratio
-  return `${html.slice(0, html.length - 14)}<meta property="fc:frame:image:aspect_ratio" content="1:1"/></head></html>`;
+  const res = `${html.slice(0, html.length - 14)}<meta property="fc:frame:image:aspect_ratio" content="1:1"/></head></html>`;
+  console.log(res);
+  return res;
 }
