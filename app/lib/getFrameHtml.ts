1ef5e764c6d2afce842c0e36680a9a28a448aa53
horsefacts <webmaster@terminally.online>
2024-02-08 21:21:12 -0500

    fix: add required OG tags

--- app/lib/getFrameHtml.ts
+++ app/lib/getFrameHtml.ts
@@ -2,8 +2,15 @@ import { FrameMetadataType, getFrameHtmlResponse } from '@coinbase/onchainkit';
 
 export function getFrameHtml(frameMetadata: FrameMetadataType) {
   const html = getFrameHtmlResponse(frameMetadata);
-  // hack: remove close tags and add aspect ratio
-  const res = `${html.slice(0, html.length - 14)}<meta property="fc:frame:image:aspect_ratio" content="1:1"/></head></html>`;
+
+  const extraTags = [
+    '<meta property="og:title" content="Farcaster: Horse">',
+    '<meta property="og:description" content="Farcaster Protocol Release">',
+    `<meta property="og:image" content="https://mint.farcaster.xyz/api/images/start?date=${Date.now()}">`,
+    '<meta property="fc:frame:image:aspect_ratio" content="1:1" />',
+  ];
+  // hack: remove close tags, add aspect ratio and required OG tags
+  const res = `${html.slice(0, html.length - 14)}${extraTags.join('')}</head></html>`;
   console.log(res);
   return res;
 }
