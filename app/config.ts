0685d92831e287ec4132cd05239c0710a6f444cc
horsefacts <webmaster@terminally.online>
2024-02-08 22:03:31 -0500

    fix: use lowercase collection address

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 export const NEXT_PUBLIC_URL = 'https://mint.farcaster.xyz';
 export const ALLOWED_ORIGIN = 'mint.farcaster.xyz';
-export const ZORA_COLLECTION_ADDRESS = '0xF5a3b6DEE033AE5025E4332695931CaDeB7f4D2B';
+export const ZORA_COLLECTION_ADDRESS = '0xf5a3b6dee033ae5025e4332695931cadeb7f4d2b';
 export const ZORA_TOKEN_ID = '1';
 export const CARD_DIMENSIONS = {
   width: 800,
