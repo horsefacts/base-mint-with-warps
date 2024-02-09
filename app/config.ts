9beeb1599283ee597ecddcc8b278092fb0c76df9
horsefacts <webmaster@terminally.online>
2024-02-08 20:19:43 -0500

    chore: update domain

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
-export const NEXT_PUBLIC_URL = 'https://base-mint-with-warps.vercel.app';
-export const ALLOWED_ORIGIN = 'base-mint-with-warps.vercel.app';
+export const NEXT_PUBLIC_URL = 'https://mint.farcaster.xyz';
+export const ALLOWED_ORIGIN = 'mint.farcaster.xyz';
 export const ZORA_COLLECTION_ADDRESS = '0xF5a3b6DEE033AE5025E4332695931CaDeB7f4D2B';
 export const ZORA_TOKEN_ID = '1';
 export const CARD_DIMENSIONS = {
